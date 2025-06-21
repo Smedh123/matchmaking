22c3107c68d2e1a970f8929e891b2a1af9d0490e
jangirsamarthiqol <samarth@truestate.in>
2025-06-21 12:53:15 +0530

    new branch

--- src/helper/Fetchers.js
+++ src/helper/Fetchers.js
@@ -1,5 +1,5 @@
 import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
-import { db } from '../firebase'; 
+import { db } from '../../firebase'; 
 
 export const fetchProperties = async () => {
   try {
